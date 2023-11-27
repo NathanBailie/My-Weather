fef0c0d3d05156788ce13aa862784b1152e1ac58
Nathan <antnefedov33@gmail.com>
2023-11-27 11:26:45 +0300

    added global theme variables, made header styles

--- src/entities/Header/ui/Header.tsx
+++ src/entities/Header/ui/Header.tsx
@@ -10,7 +10,7 @@ export const Header = memo(() => {
 
     return (
         <div className={classNames(cls.Header, {}, [])}>
-            <h1>My Weather</h1>
+            <h1>{t('MyWeather')}</h1>
             <div className={cls.Header__wrapper}>
                 <ThemeSwitcher />
                 <LangSwitcher />
--- src/widgets/LangSwitcher/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/LangSwitcher.tsx
@@ -2,6 +2,7 @@ import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Button, ButtonSizes } from 'shared/ui/Button/Button';
+import cls from './LangSwitcher.module.scss';
 
 export const LangSwitcher = memo(() => {
     const { t, i18n } = useTranslation();
@@ -14,7 +15,7 @@ export const LangSwitcher = memo(() => {
 
     return (
         <Button
-            className={classNames('', {}, ['langBtn'])}
+            className={classNames(cls.LangSwitcher, {}, ['langBtn'])}
             title={t('LangTitle')}
             onClick={changeLnguage}
             size={ButtonSizes.SWITCHER_LANG}
