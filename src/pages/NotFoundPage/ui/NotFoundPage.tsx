43f2d9715e1a7a133af18d9bf9d1ecc78acd6fb3
Nathan <antnefedov33@gmail.com>
2023-12-08 13:10:56 +0300

    updated not found page

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,10 +1,21 @@
 import { classNames } from 'shared/lib/classNames/classNames';
+import errorLight from 'shared/assets/icons/errorLight.png';
+import errorDark from 'shared/assets/icons/errorDark.png';
+import { Icon } from 'shared/ui/Icon/Icon';
 import cls from './NotFoundPage.module.scss';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { BackToList } from 'widgets/BackToList/BackToList';
 
 export const NotFoundPage = () => {
+    const { theme } = useTheme();
+
+    const errorImage = theme === 'light' ? errorLight : errorDark;
+
     return (
         <div className={classNames(cls.NotFoundPage, {}, [])}>
-            <h1>The page did not found</h1>
+            <Icon src={errorImage} alt='error_image' />
+            <h2>The page did not found</h2>
+            <BackToList />
         </div>
     );
 };
